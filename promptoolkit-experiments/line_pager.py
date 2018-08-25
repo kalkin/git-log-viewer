9ef3c688fbd7a3d568cfe152ad77882c3974f800
Bahtiar Gadimov <bahtiar@dia-com.de>
2018-08-25 16:09:16 +0200

    Clean up line pager example

--- promptoolkit-experiments/line_pager.py
+++ promptoolkit-experiments/line_pager.py
@@ -1,69 +1,62 @@
 #!/usr/bin/env python3
 
+# pylint: disable=missing-docstring
+
 import os
 
 from prompt_toolkit.application import Application
 from prompt_toolkit.application.current import get_app
-from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.key_binding import KeyBindings
-from prompt_toolkit.key_binding.bindings.focus import (focus_next,
-                                                       focus_previous)
-from prompt_toolkit.layout.containers import Float, HSplit, VSplit
-from prompt_toolkit.layout.dimension import D
+from prompt_toolkit.layout.containers import HSplit
 from prompt_toolkit.layout.layout import Layout
-from prompt_toolkit.layout.menus import CompletionsMenu
-from prompt_toolkit.lexers import PygmentsLexer
-from prompt_toolkit.styles import Style
-from prompt_toolkit.widgets import (Box, Button, Checkbox, Dialog, Frame,
-                                    Label, MenuContainer, MenuItem,
-                                    ProgressBar, RadioList, TextArea)
+from prompt_toolkit.widgets import TextArea
 # pylint: disable=no-name-in-module
-from pygit2 import Commit, Repository, discover_repository
-from pygments.lexers.html import HtmlLexer
+from pygit2 import Repository, discover_repository
 
 GIT_DIR = discover_repository(os.getcwd())
 REPO = Repository(GIT_DIR)
 ROOT = REPO.revparse_single("HEAD")
 
-textfield = TextArea(lexer=PygmentsLexer(HtmlLexer), read_only=True)
+TEXTFIELD = TextArea(read_only=True)
 # Global key bindings.
 bindings = KeyBindings()
 
 
 @bindings.add('c-c')
-def _(event):
+def _(_):
     get_app().exit(result=False)
+
 
 FIRST = True
 
+
 @bindings.add('c-a')
-def foo(event):
+def foo(_):
     global ROOT
     global FIRST
     if not FIRST:
         ROOT = ROOT.parents[0]
         msg = ROOT.message.strip().splitlines()[0]
-        textfield.text += "\n" + msg
+        TEXTFIELD.text += "\n" + msg
     else:
         msg = ROOT.message.strip().splitlines()[0]
-        textfield.text = msg
+        TEXTFIELD.text = msg
         FIRST = False
 
 
-
-root_container = HSplit([
-    textfield,
+ROOT_CONTAINER = HSplit([
+    TEXTFIELD,
 ])
 
-application = Application(
-    layout=Layout(root_container, ),
+APPLICATION = Application(
+    layout=Layout(ROOT_CONTAINER, ),
     key_bindings=bindings,
     mouse_support=True,
     full_screen=True)
 
 
 def run():
-    result = application.run()
+    result = APPLICATION.run()
     print('You said: %r' % result)
 
 
