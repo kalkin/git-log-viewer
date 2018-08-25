af1971121ee69d3c389b59b3c75a9b4070dec5eb
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2018-08-25 20:45:35 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+""" pygit-viewer package specification """
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="pygit-viewer",
+    version="0.0.1",
+    author="Bahtiar `kalkin` Gadimov",
+    author_email="bahtiar@gadimov.de",
+    description="A git log viewer with folding support",
+    url="git@github.com:monorepo.git:/pygit-viewer",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    entry_points='''
+        [console_scripts]
+        pygit-viewer=pygit_viewer.main:cli
+    ''',
+    install_requires=[
+        'pygit2', 'prompt_toolkit >= 2.0, <=3.0', 'Babel >= 2.5.1, <=3.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+):",
+        "Operating System :: POSIX",
+        "Topic :: Software Development :: Version Control :: Git",
+    ],
+)
