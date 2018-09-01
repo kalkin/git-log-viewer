db49e24667e30617cca8b25003c79d62db502149
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2018-09-01 20:51:32 +0200

    Update features & steps

--- features/first.feature
+++ features/first.feature
@@ -1,15 +1,28 @@
 Feature: Repository Walker
-  In order to get commits we need an abstraction layer for the `pygit2.Repositor`.
+    In order to get commits we need an abstraction layer for the `pygit2.Repositor`.
 
-  Scenario: Iterate over part of repository
-    Given a repository in current working directory
-      And starting commit HEAD
-      And last commit HEAD~10
-     When I walk over commits
-     Then I should have iterated over 10 commits
+    Scenario: Count commits between HEAD & HEAD~10
+        Given commit START (HEAD)
+        And commit LAST (HEAD~10)
+        And a walker over commits between START & LAST
+        When I walk over commits
+        Then I should have iterated over 10 commits
 
-  Scenario: Iterate over part2 of repository
-    Given starting commit HEAD~3
-      And last commit HEAD~10
-     When I walk over commits
-     Then I should have iterated over 7 commits
+    Scenario: Count commits between HEAD~3 & HEAD~10
+        Given commit START (HEAD~3)
+        And commit LAST (HEAD~10)
+        And a walker over commits between START & LAST
+        When I walk over commits
+        Then I should have iterated over 7 commits
+
+    Scenario: Iterate over commits of the first parent
+        Given foldable commit ce537d3
+        And a walker over commit children
+        When I walk over commits
+        Then I should have iterated over 3 commit
+
+    Scenario: Find merge base
+        Given commit A (fc0f00a)
+        And commit B (5126d60)
+        When I calculate merge base of A & B
+        Then the result commit should be 5126d60
--- features/steps.py
+++ features/steps.py
@@ -1,15 +1,26 @@
 # pylint: disable=missing-docstring,invalid-name,unused-argument
 import os
 
-from lettuce import step, world
-from pygit_viewer.line import Repo
+from lettuce import after, before, step, world
+from pygit_viewer.line import Repo, Foldable, Commit
 
 
-@step('(?:Given|And) a repository in (.*)')
-def init_repo(step, path: str):
-    path: str = parse_path(path)
+@before.each_scenario
+def init_repo(scenario):
     world.repo = Repo(os.getcwd())
-    assert world.repo, 'Get a Repo instance'
+    world.commits = {}
+
+
+@after.each_scenario
+def unset_world(scenario):
+    world.repo = None
+    world.walker = None
+    world.commit = None
+    try:
+        del world.result
+    except Exception:  # pylint: disable=broad-except
+        pass
+
 
 def parse_path(text: str) -> str:
     if text == 'current working directory':
@@ -17,29 +28,58 @@ def parse_path(text: str) -> str:
     else:
         assert False, 'Not implemented yet.'
 
-@step('(?:Given|And) starting commit (.*)')
-def define_start_commit(step, oid: str):
-    world.start = oid
-    assert world.start, 'Got oid'
 
-
-@step('And last commit (.*)')
-def define_last_commit(step, oid: str):
-    world.last = oid
-    assert world.last, 'No oid provided'
+@step(r'(?:a )?walker over commits between (\w+) & (\w+)')
+def walker_over_commits(_, first, last):
+    start = world.commits[first]
+    end = world.commits[last]
+    world.walker = world.repo.walker(start, end)
 
 
 @step('When I walk over commits')
-def b_when_i_walk_over_commits(step):
-    world.walker = world.repo.walker(world.start, world.last)
-    assert world.walker, 'Got walker'
+def walk_over_commits(_):
+    world.result = 0
+    for _ in world.walker:
+        # print(commit)
+        world.result += 1
+    assert world.result > 0
 
 
-@step(u'Then I should have iterated over (\d+) commits')
-def b_then_i_should_have_iterate_over_10_commits(step, expected):
-    expected = int(expected)
-    actual = 0
-    for commit in world.walker:
-        actual += 1
+@step(r'Then I should have iterated over (\d+) commits?')
+def assert_number_of_commits(_, expected):
+    assert world.result == int(expected), 'Expected: ' + str(
+        expected) + ' / Got: ' + str(world.result)
 
-    assert actual == expected, 'Expected: ' + str(expected) + ' / Got: ' + str(actual)
+
+@step(r'foldable commit (\w+)')
+def foldable_commit(_, sth):
+    world.commit = world.repo.get(sth)
+    assert isinstance(world.commit, Foldable), 'A foldable commit'
+
+
+@step(r'commit (\w+) \(([\w~]+)\)')
+def any_commit(_, name, sth):
+    world.commits[name] = world.repo.get(sth)
+    assert isinstance(world.commits[name], Commit), 'Got commit ' + str(sth)
+
+
+@step(u'And a walker over commit children')
+def children_walker(_):
+    world.walker = world.commit.child_log()
+
+
+@step(r'I calculate merge base of (\w+) & (\w+)')
+def merge_base(_, a, b):
+    a = world.commits[a]
+    b = world.commits[b]
+
+    # pylint: disable=protected-access
+    world.result = world.repo.merge_base(a._commit, b._commit)
+    assert isinstance(world.result, Commit), 'Got commit ' + str(world.result)
+
+
+@step(r'Then the result commit should be (\w+)')
+def compare_commit(self, sth):
+    expected = world.repo.get(sth).oid
+    actual = world.result.oid
+    assert expected == actual, 'Got {}' % actual
