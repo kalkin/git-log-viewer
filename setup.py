e73bb2ef0528f1bc788d7d6afdbfb6ba0a2b6c83
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2020-04-12 20:33:54 +0200

    build: add pygments >= 2.6.0 to the requirements

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     install_requires=[
         'pygit2 >= 0.28.0', 'prompt_toolkit >= 2.0, <4.0',
         'Babel >= 2.5.1, <3.0', 'certifi', 'urllib3', 'docopt',
-        'pykka >= 2.0.0'
+        'pykka >= 2.0.0', 'pygments >= 2.6.0'
     ],
     tests_require=['aloe >= 0.1.19, <= 0.2.0'],
     classifiers=[
