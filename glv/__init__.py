4910a0c81e8f0c3f7fafcfc90c2dbaeeed335f49
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2020-04-13 18:39:48 +0200

    improvement: Add Repo.__str__() function

--- glv/__init__.py
+++ glv/__init__.py
@@ -267,6 +267,7 @@ class LogEntry:
             return ('bold', '⮉ ')
         elif re.match(r'^style:?\s*', subject, flags=re.I):
             return ('bold', '♥ ')
+
         return ('', '  ')
 
     @property
