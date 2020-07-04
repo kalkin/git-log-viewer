b9923d90aed3a1b1e41820f348959ce75018e593
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2020-07-04 12:54:26 +0200

    improvement: Diff view highlight the subject

--- glv/main.py
+++ glv/main.py
@@ -332,6 +332,7 @@ def patched_style() -> Style:
         ('pygments.commit.filename', 'ansiblue'),
         ('pygments.commit.refs', 'ansiyellow'),
         ('pygments.commit.modules', 'ansiyellow'),
+        ('pygments.commit.subject', 'bold'),
         ('pygments.commit.diffstart', 'bold'),
     ]
     return style
