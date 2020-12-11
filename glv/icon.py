2edc6d86e1a7ca1a986f311b0ad30767146a05a1
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2020-12-11 15:11:01 +0100

    improvement(icons): Extend ASCII icons

--- glv/icon.py
+++ glv/icon.py
@@ -20,14 +20,16 @@
 
 ASCII = [
     (r'^Revert:?\s*', 'R '),
-    (r'^fixup!\s+', 'f '),
-    (r'^(hot|bug)?fix(ing|ed)?(\(.+\))?[\/:\s]+', 'B '),  # B for bug
+    (r'^fixup!\s+', 'F '),
+    (r'^(hot|bug)?fix(ing|ed)?(\(.+\))?[\/:\s]+', 'f '),  # B for bug
     (r'^add(ed)?(:|\s)', '+ '),
     (r'^feat:?\s*', '+ '),
     (r'^build:?\s*', 'b '),
     (r'^doc(s|umentation)?:?\s*', 'D '),
     (r'^style:?\s*', 's '),
     (r'^test(\(.+\))?:?\s*', 'T '),
+    (r'^improvement:?\s*', '* '),
+    (r'^refactor:?\s*', '8 '),
 ]
 
 NERDFONT = [
