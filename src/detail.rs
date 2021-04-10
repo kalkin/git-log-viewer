4ee9578a625d97cd607da52a27faf5e45b650b1d
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2021-04-10 13:34:23 +0200

    Improve diff view

--- src/detail.rs
+++ src/detail.rs
@@ -1,11 +1,16 @@
 use cursive::event::{Event, EventResult};
+use cursive::theme::Style;
 use cursive::traits::*;
+use cursive::utils::span::SpannedString;
 use cursive::views::{ScrollView, TextContent, TextView};
 use cursive::{Printer, Vec2, View};
 
 use glv_core::Commit;
+use glv_core::Oid;
 
 use crate::raw;
+use crate::style::{bold_style, date_style, DEFAULT_STYLE};
+use crate::style::{id_style, name_style};
 use crate::views::DetailView;
 
 pub struct CommitDetailView {
@@ -40,24 +45,83 @@ impl View for CommitDetailView {
 
 impl DetailView for CommitDetailView {
     fn set_detail(&mut self, detail: &Commit) {
-        let tmp = git_log(&detail.id().0);
-        self.content = Some(TextView::new_with_content(tmp).scrollable());
+        let content = TextContent::new("");
+        content.append(color_span(
+            "Commit:          ",
+            &detail.id().0,
+            id_style(&DEFAULT_STYLE),
+        ));
+
+        content.append(color_span(
+            "Author:          ",
+            &detail.author_name(),
+            name_style(&DEFAULT_STYLE),
+        ));
+
+        content.append(color_span(
+            "Author Date:     ",
+            &detail.author_date(),
+            date_style(&DEFAULT_STYLE),
+        ));
+
+        // Committer lines {
+        if detail.author_name() != detail.committer_name() {
+            content.append(color_span(
+                "Committer:       ",
+                &detail.author_name(),
+                name_style(&DEFAULT_STYLE),
+            ));
+        }
+        if detail.author_date() == detail.committer_date() {
+            content.append(color_span(
+                "Committer Date:  ",
+                &detail.committer_date(),
+                date_style(&DEFAULT_STYLE),
+            ));
+        }
+        // Committer lines }
+
+        content.append("\n");
+        content.append(SpannedString::styled(
+            format!(" {}\n", detail.subject()),
+            bold_style(&DEFAULT_STYLE),
+        ));
+        content.append("\n");
+        for line in detail.body().lines() {
+            content.append(format!(" {}\n", line));
+        }
+        content.append("                                 ❦ ❦ ❦ ❦ \n\n");
+        for s in git_diff(detail) {
+            content.append(s);
+        }
+        self.content = Some(TextView::new_with_content(content).scrollable());
     }
 }
 
-fn git_log(id: &str) -> TextContent {
+fn color_span(key: &str, content: &str, style: Style) -> SpannedString<Style> {
+    let line = format!("{}{}\n", key, content);
+    SpannedString::styled(line, style)
+}
+
+fn git_diff(commit: &Commit) -> Vec<SpannedString<Style>> {
     let working_dir = &git_wrapper::top_level().unwrap()[..];
+    let default = Oid { 0: "".to_string() };
+    let bellow = commit.bellow().unwrap_or(&default);
+    let rev = format!("{}..{}", commit.id().0, bellow.0);
     let proc = git_wrapper::git_cmd_out(
         working_dir.to_string(),
-        vec!["log", "-1", "-p", "--color=always", id],
+        vec![
+            "diff",
+            "--stat",
+            "-p",
+            "-M",
+            "--color=always",
+            "--full-index",
+            &rev,
+        ],
     )
     .unwrap();
 
     let stdout: Vec<u8> = proc.stdout;
-
-    let content = TextContent::new("");
-    for line in raw::parse_spans(stdout) {
-        content.append(line);
-    }
-    content
+    raw::parse_spans(stdout)
 }
