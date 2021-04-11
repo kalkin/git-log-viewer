5f9eeaa8e48453a36f21ac68f87342fd6d35c0cb
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2021-04-11 18:14:58 +0200

    feat: Handle -w & <references> args

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use crate::detail::CommitDetailView;
 use crate::scroll::CustomScrollView;
 use crate::views::DynamicSplitView;
 
+use docopt::Docopt;
+use serde::Deserialize;
+
 mod detail;
 mod history;
 mod raw;
@@ -12,10 +15,34 @@ mod scroll;
 mod views;
 mod style;
 
+const USAGE: &str = "
+glv - Git Log Viewer a TUI application with support for folding merges
+
+Usage:
+    glv [-w DIR|--workdir=DIR] [<revision>] [ -h | --help ]
+
+Options:
+    -w DIR, --workdir=DIR   Directory where the git repository is.
+    -h, --help              Show this usage.
+
+Arguments:
+    <revision>                A branch, tag or commit
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_workdir: Option<String>,
+    arg_revision: Option<String>,
+}
+
 fn main() {
     cursive::logger::init();
     // Creates the cursive root - required for every application.
     let mut siv = Cursive::new();
+
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
 
     //let tmp = git_log();
     //let content = TextContent::new("");
@@ -27,8 +54,9 @@ fn main() {
     //.full_width()
     //.scrollable();
 
-    let working_dir = git_wrapper::top_level().unwrap();
-    let history = history::History::new(&working_dir, "HEAD").unwrap();
+    let working_dir = args.flag_workdir.unwrap_or(git_wrapper::top_level().unwrap());
+    let revision = args.arg_revision.unwrap_or("HEAD".to_string());
+    let history = history::History::new(&working_dir, &revision).unwrap();
     // let main = CustomScrollView::new(history);
     let main = CustomScrollView::new(history);
     let aside = CommitDetailView::new();
