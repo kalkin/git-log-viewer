bcc6c95352bda6829a914f2c1ebd6ff24c798d1a
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2022-01-17 22:47:21 +0100

    change(diff_view): Highlight subtrees in yellow

--- src/detail.rs
+++ src/detail.rs
@@ -24,7 +24,7 @@ use git_wrapper::Repository;
 
 use crate::commit::Commit;
 use crate::commit::Oid;
-use crate::default_styles::{DATE_STYLE, DEFAULT_STYLE, ID_STYLE, NAME_STYLE};
+use crate::default_styles::{DATE_STYLE, DEFAULT_STYLE, ID_STYLE, NAME_STYLE, MOD_STYLE};
 use crate::history_entry::HistoryEntry;
 use crate::raw;
 use crate::ui::base::data::StyledAreaAdapter;
@@ -85,7 +85,7 @@ impl DetailsWidget<HistoryEntry> for DiffView {
             data.push(color_text(
                 "Strees:          ",
                 &module_names.join(", "),
-                *DATE_STYLE,
+                *MOD_STYLE,
             ));
         }
 
